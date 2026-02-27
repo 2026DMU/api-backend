4ed95b9b9828b14f33b0dd8246e05dae0ca6f7ff
Backhoe312 <idb591575@gmail.com>
2026-02-27 20:06:49 +0900

    init: project structure

---
+++ src/main/java/com/weathercody/api/WeathercodyApiApplication.java
@@ -0,0 +1,13 @@
+package com.weathercody.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeathercodyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeathercodyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/weathercody/api/WeathercodyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weathercody.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeathercodyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
