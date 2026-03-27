469b232f2b7ec0ef9378e3f21cdcc340f100db39
김민혁 <royalkim0707@gmail.com>
2026-03-27 21:26:41 +0900

    도커 로컬 실행환경 및 swagger 인증 테스트 설정 추가

--- src/main/java/com/weathercody/api/config/SecurityConfig.java
+++ src/main/java/com/weathercody/api/config/SecurityConfig.java
@@ -13,24 +13,23 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    //  BCryptPasswordEncoder(비밀번호 암호화)
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                // CSRF 보호 비활성화
                 .csrf(AbstractHttpConfigurer::disable)
-
-
                 .authorizeHttpRequests(auth -> auth
-                        // 회원가입 및 로그인 API는 모든 사용자에게 접근 허용
-                        .requestMatchers("/api/auth/signup", "/api/auth/login").permitAll()
-                        // 그 외의 모든 요청은 인증 필요
+                        .requestMatchers(
+                                "/api/auth/**",
+                                "/error",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/v3/api-docs/**"
+                        ).permitAll()
                         .anyRequest().authenticated()
                 );
 
